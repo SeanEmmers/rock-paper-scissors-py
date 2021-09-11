10d73dc4004dff2d59e5985e78c7b624e802c315
Sean Phillips <seanphillips9696@gmail.com>
2021-09-11 12:56:55 +0200

    setting up while loop and quit

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -5,3 +5,7 @@ computer_wins = 0
 
 options = ["rock", "paper", "scissors"]
 
+while True:
+  user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+  if user_input == "q":
+    break
